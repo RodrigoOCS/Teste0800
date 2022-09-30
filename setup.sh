ab85f87ae140f23d2fb9d74498153a384f9b276e
RodrigoOCS <114358922+RodrigoOCS@users.noreply.github.com>
2022-09-29 23:55:43 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"rodigocelular@gmail.com\"\n\
+email = \"rodrigodecarvalhoesilva@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
